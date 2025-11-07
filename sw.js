80e55ded8df0e374897e760d166dfbcb69207cef
xiaozhenlei <93065862+xiaozhenlei@users.noreply.github.com>
2025-11-07 22:12:27 +0800

    第一次测试
    第一次测试

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'mossia-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          return response || fetch(event.request);
+        })
+    );
+  }
+});
